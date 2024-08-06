3661e92a2699f9b291b9084c081d2509abd2d1ae
EMIL_VALIULLIN <emil.valiullin@simbirsoft.com>
2024-08-06 14:53:42 +0300

    Создание класс корабля и добавление изображения

---
+++ alien-invasion/settings.py
@@ -0,0 +1,7 @@
+class Settings:
+    """Класс для хранения всех настроек игры Alien Invasion."""
+    def __init__(self):
+        """Инициализирует настройки игры."""
+        self.screen_width = 2540
+        self.screen_height = 1360
+        self.bg_color = (230, 230, 230)
